754f74addb2329bcdae66c967c4a2caa28abca8a
Timothy McCarthy <ebh042@gmail.com>
2020-06-05 18:09:31 +1000

    Add implementations for some of the NonEmptyMap instances (WiP)

--- cats/src/main/scala/au/id/tmm/collections/cats/instances/NonEmptyMapInstances.scala
+++ cats/src/main/scala/au/id/tmm/collections/cats/instances/NonEmptyMapInstances.scala
@@ -1,67 +1,72 @@
 package au.id.tmm.collections.cats.instances
 
 import au.id.tmm.collections.NonEmptyMap
-import cats.arrow.Compose
 import cats.data.Ior
-import cats.{
-  Align,
-  CommutativeApplicative,
-  FlatMap,
-  Functor,
-  FunctorFilter,
-  Semigroup,
-  SemigroupK,
-  Show,
-  UnorderedTraverse,
-}
+import cats.instances.map._
 import cats.kernel.{CommutativeMonoid, CommutativeSemigroup, Eq, Hash}
+import cats.{Align, CommutativeApplicative, FlatMap, Functor, FunctorFilter, Semigroup, Show, UnorderedTraverse}
 
 trait NonEmptyMapInstances extends NonEmptyMapInstances1 {
 
-  implicit def tmmCollectionsHashForNonEmptyMap[K, V : Hash]: Hash[NonEmptyMap[K, V]] =
+  implicit def tmmCollectionsHashForNonEmptyMap[K : Hash, V : Hash]: Hash[NonEmptyMap[K, V]] =
     new Hash[NonEmptyMap[K, V]] {
-      override def hash(x: NonEmptyMap[K, V]): Int                          = ???
-      override def eqv(x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): Boolean = ???
+      override def hash(x: NonEmptyMap[K, V]): Int = Hash[Map[K, V]].hash(x.underlying)
+
+      override def eqv(x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): Boolean =
+        Hash[Map[K, V]].eqv(x.underlying, y.underlying)
     }
 
   implicit def show[K : Show, V : Show]: Show[NonEmptyMap[K, V]] =
     new Show[NonEmptyMap[K, V]] {
-      override def show(t: NonEmptyMap[K, V]): String = ???
+      override def show(map: NonEmptyMap[K, V]): String =
+        map.iterator
+          .map { case (a, b) => Show[K].show(a) + " -> " + Show[V].show(b) }
+          .mkString("NonEmptyMap(", ", ", ")")
     }
 
   implicit def tmmCollectionsStdInstancesForNonEmptyMap[
     K,
   ]: UnorderedTraverse[NonEmptyMap[K, *]] with FlatMap[NonEmptyMap[K, *]] with Align[NonEmptyMap[K, *]] =
     new UnorderedTraverse[NonEmptyMap[K, *]] with FlatMap[NonEmptyMap[K, *]] with Align[NonEmptyMap[K, *]] {
-      override def unorderedTraverse[G[_], A, B](sa: NonEmptyMap[K, A])(f: A => G[B])(implicit evidence$1: CommutativeApplicative[G]): G[NonEmptyMap[K, B]] = ???
+      override def unorderedTraverse[G[_], A, B](
+        sa: NonEmptyMap[K, A],
+      )(
+        f: A => G[B],
+      )(implicit
+        evidence$1: CommutativeApplicative[G],
+      ): G[NonEmptyMap[K, B]] = ???
 
-      override def flatMap[A, B](fa: NonEmptyMap[K, A])(f: A => NonEmptyMap[K, B]): NonEmptyMap[K, B] = ???
+      override def flatMap[A, B](fa: NonEmptyMap[K, A])(f: A => NonEmptyMap[K, B]): NonEmptyMap[K, B] =
+        fa.flatMap {
+          case (k, a) => f(a)
+        }
 
-      override def tailRecM[A, B](a: A)(f: A => NonEmptyMap[K, Either[A, B]]): NonEmptyMap[K, B] = ???
+      override def tailRecM[A, B](a: A)(f: A => NonEmptyMap[K, Either[A, B]]): NonEmptyMap[K, B] =
+        NonEmptyMap.fromMapUnsafe(FlatMap[Map[K, *]].tailRecM(a)(f.andThen(_.underlying)))
 
-      override def functor: Functor[NonEmptyMap[K, *]] = ???
+      override def functor: Functor[NonEmptyMap[K, *]] = this
 
       override def align[A, B](fa: NonEmptyMap[K, A], fb: NonEmptyMap[K, B]): NonEmptyMap[K, Ior[A, B]] = ???
 
-      override def unorderedFoldMap[A, B](fa: NonEmptyMap[K, A])(f: A => B)(implicit evidence$1: CommutativeMonoid[B]): B = ???
+      override def unorderedFoldMap[A, B](
+        fa: NonEmptyMap[K, A],
+      )(
+        f: A => B,
+      )(implicit
+        evidence$1: CommutativeMonoid[B],
+      ): B = ???
 
-      override def map[A, B](fa: NonEmptyMap[K, A])(f: A => B): NonEmptyMap[K, B] = ???
+      override def map[A, B](fa: NonEmptyMap[K, A])(f: A => B): NonEmptyMap[K, B] =
+        fa.map {
+          case (k, a) => k -> f(a)
+        }
     }
-
-  implicit val tmmCollectionsComposeForNonEmptyMap: Compose[NonEmptyMap] = new Compose[NonEmptyMap] {
-    override def compose[A, B, C](f: NonEmptyMap[B, C], g: NonEmptyMap[A, B]): NonEmptyMap[A, C] = ???
-  }
 
   implicit def tmmCollectionsFunctorFilterForNonEmptyMap[K]: FunctorFilter[NonEmptyMap[K, *]] =
     new FunctorFilter[NonEmptyMap[K, *]] {
-      override def functor: Functor[NonEmptyMap[K, *]] = ???
-      override def mapFilter[V1, V2](fa: NonEmptyMap[K, V1])(f: V1 => Option[V2]): NonEmptyMap[K, V2] = ???
-    }
+      override def functor: Functor[NonEmptyMap[K, *]] = tmmCollectionsStdInstancesForNonEmptyMap
 
-  implicit def tmmCollectionsSemigroupKForNonEmptyMap[K]: SemigroupK[NonEmptyMap[K, *]] =
-    new SemigroupK[NonEmptyMap[K, *]] {
-      override def combineK[V](x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): NonEmptyMap[K, V] = ???
-      override def algebra[V]: Semigroup[NonEmptyMap[K, V]] = ???
+      override def mapFilter[V1, V2](fa: NonEmptyMap[K, V1])(f: V1 => Option[V2]): NonEmptyMap[K, V2] = ???
     }
 
   implicit def tmmCollectionsCommutativeSemigroupForNonEmptyMap[
@@ -69,7 +74,8 @@ trait NonEmptyMapInstances extends NonEmptyMapInstances1 {
     V : CommutativeSemigroup,
   ]: CommutativeSemigroup[NonEmptyMap[K, V]] =
     new CommutativeSemigroup[NonEmptyMap[K, V]] {
-      override def combine(x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): NonEmptyMap[K, V] = ???
+      override def combine(x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): NonEmptyMap[K, V] =
+        tmmCollectionsSemigroupForNonEmptyMap[K, V].combine(x, y)
     }
 
 }
@@ -78,21 +84,30 @@ private[instances] trait NonEmptyMapInstances1 extends NonEmptyMapInstances2 {
 
   implicit def tmmCollectionsEqForNonEmptyMap[K, V : Eq]: Eq[NonEmptyMap[K, V]] =
     new Eq[NonEmptyMap[K, V]] {
-      override def eqv(x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): Boolean = ???
+      override def eqv(x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): Boolean =
+        Eq[Map[K, V]].eqv(x.underlying, y.underlying)
     }
 
   implicit def tmmCollectionsSemigroupForNonEmptyMap[K, V : Semigroup]: Semigroup[NonEmptyMap[K, V]] =
     new Semigroup[NonEmptyMap[K, V]] {
-      override def combine(x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): NonEmptyMap[K, V] = ???
+      override def combine(x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): NonEmptyMap[K, V] =
+        NonEmptyMap.fromMapUnsafe(Semigroup[Map[K, V]].combine(x.underlying, y.underlying))
+
+      override def combineAllOption(as: IterableOnce[NonEmptyMap[K, V]]): Option[NonEmptyMap[K, V]] =
+        Semigroup[Map[K, V]].combineAllOption(as.iterator.map(_.underlying)).map(NonEmptyMap.fromMapUnsafe)
     }
 
 }
 
 private[instances] trait NonEmptyMapInstances2 {
 
-  implicit def tmmCollectionsFunctorOverKeysForNonEmptyMap[K, V : Semigroup]: Semigroup[NonEmptyMap[K, V]] =
-    new Semigroup[NonEmptyMap[K, V]] {
-      override def combine(x: NonEmptyMap[K, V], y: NonEmptyMap[K, V]): NonEmptyMap[K, V] = ???
+  implicit def tmmCollectionsFunctorOverKeysForNonEmptyMap[V : Semigroup]: Functor[NonEmptyMap[*, V]] =
+    new Functor[NonEmptyMap[*, V]] {
+
+      import au.id.tmm.collections.cats.instances.mapOverKeys._
+
+      override def map[A, B](fa: NonEmptyMap[A, V])(f: A => B): NonEmptyMap[B, V] =
+        NonEmptyMap.fromMapUnsafe(Functor[Map[*, V]].map(fa.underlying)(f))
     }
 
 }
--- cats/src/test/scala/au/id/tmm/collections/cats/instances/NonEmptyMapInstancesSpec.scala
+++ cats/src/test/scala/au/id/tmm/collections/cats/instances/NonEmptyMapInstancesSpec.scala
@@ -1,21 +1,54 @@
 package au.id.tmm.collections.cats.instances
 
 import au.id.tmm.collections.NonEmptyMap
-import au.id.tmm.collections.scalacheck.nonEmptyMap._
+import au.id.tmm.collections.cats.instances.NonEmptyMapInstancesSpec.EqOnlyInt
 import au.id.tmm.collections.cats.instances.nonEmptyMap._
-import au.id.tmm.utilities.testing.AdHocTestIgnore
-import cats.UnorderedTraverse
+import au.id.tmm.collections.scalacheck.nonEmptyMap._
 import cats.data.Validated
-import cats.kernel.laws.discipline.{EqTests, HashTests}
-import cats.laws.discipline.{FlatMapTests, UnorderedTraverseTests}
-import cats.tests.CatsSuite
+import cats.kernel.Eq
+import cats.kernel.laws.discipline.{CommutativeSemigroupTests, EqTests, HashTests, SemigroupTests}
 import cats.laws.discipline.arbitrary._
+import cats.laws.discipline.{AlignTests, FlatMapTests, FunctorFilterTests, FunctorTests, UnorderedTraverseTests}
+import cats.tests.CatsSuite
+import org.scalacheck.{Arbitrary, Cogen, Shrink}
 
 class NonEmptyMapInstancesSpec extends CatsSuite {
 
-  checkAll("Eq for tmmUtils NonEmptyMap", EqTests[NonEmptyMap[String, Int]].eqv)
+  checkAll("Eq for tmmUtils NonEmptyMap", EqTests[NonEmptyMap[String, EqOnlyInt]].eqv)
   checkAll("Hash for tmmUtils NonEmptyMap", HashTests[NonEmptyMap[String, Int]].hash)
-  checkAll("UnorderedTraverse for tmmUtils NonEmptyMap", UnorderedTraverseTests[NonEmptyMap[String, *]].unorderedTraverse[Int, Int, Int, Option, Validated[Int, *]])
+  checkAll(
+    "UnorderedTraverse for tmmUtils NonEmptyMap",
+    UnorderedTraverseTests[NonEmptyMap[String, *]].unorderedTraverse[Int, Int, Int, Option, Validated[Int, *]],
+  )
   checkAll("FlatMap for tmmUtils NonEmptyMap", FlatMapTests[NonEmptyMap[String, *]].flatMap[Int, Int, Int])
+  checkAll("Align for tmmUtils NonEmptyMap", AlignTests[NonEmptyMap[String, *]].align[Int, Int, Int, Int])
+  checkAll(
+    "FunctorFilter for tmmUtils NonEmptyMap",
+    FunctorFilterTests[NonEmptyMap[String, *]].functorFilter[Int, Int, Int],
+  )
+  checkAll(
+    "CommutativeSemigroup for tmmUtils NonEmptyMap",
+    CommutativeSemigroupTests[NonEmptyMap[String, Int]].commutativeSemigroup,
+  )
+  checkAll("Semigroup for tmmUtils NonEmptyMap", SemigroupTests[NonEmptyMap[String, Int]].semigroup)
+  checkAll(
+    "Functor over keys for tmmUtils NonEmptyMap",
+    FunctorTests[NonEmptyMap[*, Int]].functor[String, String, String],
+  )
 
+}
+
+object NonEmptyMapInstancesSpec {
+
+  /**
+    * A wrapper around Int for which there is an Eq implementation, but no Hash implementation
+    */
+  final case class EqOnlyInt(asInt: Int) extends AnyVal
+
+  object EqOnlyInt {
+    implicit val eq: Eq[EqOnlyInt]               = _.asInt == _.asInt
+    implicit val arbitrary: Arbitrary[EqOnlyInt] = Arbitrary(Arbitrary.arbitrary[Int].map(EqOnlyInt.apply))
+    implicit val cogen: Cogen[EqOnlyInt]         = Cogen[Int].contramap(_.asInt)
+    implicit val shrink: Shrink[EqOnlyInt]       = Shrink(x => Shrink.shrink(x.asInt).map(EqOnlyInt.apply))
+  }
 }
